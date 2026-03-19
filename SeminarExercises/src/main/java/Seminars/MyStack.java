4a63b54def2db7648a36dfee185a4cbb83d1b504
vdeva24-AegRh <vdeva24@freeuni.edu.ge>
2026-03-19 16:04:40 +0400

    Latest

--- SeminarExercises/src/main/java/Seminars/MyStack.java
+++ SeminarExercises/src/main/java/Seminars/MyStack.java
@@ -21,7 +21,7 @@ public class MyStack<T> {
         }
         data[logLen++] = newElem;
     }
-//
+//vakho
     public T pop() {
         if(logLen == 0) {
             throw new IndexOutOfBoundsException("Stack Is Empty!");
